a6e75b342d78ea8739e17bb3abd8e5e8a61e4bd4
tstef1993 <t.stefansson93@gmail.com>
2016-01-14 20:12:11 +0100

    Lagt till category dropdownlist, med Data bind mot SQL server. Även fixat så att dropdownlist har valet "Categories" som default.

--- KodKnäckarGänget/E-Handel/Site.Master.designer.cs
+++ KodKnäckarGänget/E-Handel/Site.Master.designer.cs
@@ -31,6 +31,15 @@ namespace E_Handel {
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
         
         /// <summary>
+        /// SqlDataSource1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.SqlDataSource SqlDataSource1;
+        
+        /// <summary>
         /// SearchBox control.
         /// </summary>
         /// <remarks>
@@ -47,6 +56,15 @@ namespace E_Handel {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Button SearchButton;
+        
+        /// <summary>
+        /// DropDownList1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList DropDownList1;
         
         /// <summary>
         /// ImageButton1 control.
